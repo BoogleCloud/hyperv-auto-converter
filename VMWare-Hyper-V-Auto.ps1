5c9ba24e7296a95c345ad3b04eac46ff3bb6ac01
BoogleCloud <35645342+BoogleCloud@users.noreply.github.com>
2018-04-14 16:13:55 -0400

    Update VMWare-Hyper-V-Auto.ps1
    Successfully working with Rapid7 'Hackazon' vm. Need to test with more and clean up the code a bit.

--- VMWare-Hyper-V-Auto.ps1
+++ VMWare-Hyper-V-Auto.ps1
@@ -4,7 +4,7 @@ Import-Module 'C:\Program Files\Microsoft Virtual Machine Converter\MvmcCmdlet.p
 #.\dsfi.exe .\convert\Hackazon-disk1.vmdk 512 1024 .\descriptor.txt
 #.\dsfo.exe .\convert\Hackazon-disk1.vmdk 512 1024 descriptor.txt
 
-$path = "e:\exports\convert\sleepy-disk1.vmdk"
+$path = $args[0]
 
 $workingDir = Split-Path $path
 
@@ -12,18 +12,22 @@ $descriptor = "$workingDir\descriptor.txt"
 
 $reader = [System.IO.File]::OpenRead($path)
 $buffer = New-Object Byte[] 1024
-$reader.Seek(512, [System.IO.SeekOrigin].Begin)
+$reader.Seek(512, [System.IO.SeekOrigin]::Begin)
 $bytesRead = $reader.Read($buffer, 0, $buffer.Length)
 [System.IO.File]::WriteAllBytes($descriptor,$buffer)
+$reader.Close()
 
 (Get-Content $descriptor).replace('ddb.toolsInstallType','#ddbtoolsInstallType') | Set-Content $descriptor
 
-$reader2 = [System.IO.File]::OpenRead($descriptor)
-$bytesRead = $reader2.Read($buffer, 0, $buffer.Length)
-$reader2.Close()
+$reader = [System.IO.File]::OpenRead($descriptor)
+$bytesRead = $reader.Read($buffer, 0, $buffer.Length)
+$reader.Close()
 
-$reader.Seek(512, [System.IO.SeekOrigin].Begin)
+$reader = [System.IO.File]::OpenWrite($path)
+$reader.Seek(512, [System.IO.SeekOrigin]::Begin)
 $reader.Write($buffer, 0, $buffer.Length)
 $reader.Close()
 
-ConvertTo-MvmcVhd -SourceLiteralPath $path -vhdtype DynamicHardDisk -VhdFormat vhdx -DestinationLiteralPath $workingDir
+ConvertTo-MvmcVirtualHardDisk -SourceLiteralPath $path -vhdtype DynamicHardDisk -VhdFormat vhdx -DestinationLiteralPath $workingDir
+
+
